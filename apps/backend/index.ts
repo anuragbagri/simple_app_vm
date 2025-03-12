4780ec095613c2a3eba113c6cd01fcc3a2fe3ebc
anurag bagri <anurag123bagri@gmail.com>
2025-03-12 12:10:57 +0530

    -

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,6 +1,7 @@
 import { prismaClient } from "db/client";
 import express from "express"
 const app =express();
+app.use(express.json());
 
 
 // simple endpoints get and post
@@ -12,28 +13,33 @@ app.get("/users" ,(req,res) => {
 })
 
 
-
-app.post("/users" ,(req,res)=> {
-  const { username , password } = req.body;
-  if(!username || !password){
-    res.json({
-    "message" : "username or the password is not entered"
-    })
-    return
-  }
-  
-  const findUser = prismaClient.user.findMany({
-   where : {
-    data : {
-        username : username,
-        password : password
+app.post("/users" ,async (req,res)=> {
+    const { username , password } = req.body;
+    if(!username || !password){
+      res.json({
+      "message" : "username or the password is not entered"
+      })
+      return
     }
-   }})
-   if(!findUser){
-    const insertUser = prismaClient.user.create({
-        data : {
-            username,
-            password
-        }
-    })}
-})
+    
+    const findUser = await prismaClient.user.findMany({
+     where : {
+          username : username,
+          password : password
+      }
+     })
+     if(!findUser){
+      const insertUser =await prismaClient.user.create({
+          data : {
+              username,
+              password
+          }
+      })}
+      res.json({
+        message : "request receievd" ,
+        data : req.body
+      })
+  }) 
+  
+
+app.listen(8000 ,()=> { console.log("server running on the port")});
